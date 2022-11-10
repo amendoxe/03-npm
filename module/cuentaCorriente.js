5524dc4755635980793356e96fbf8cc97dff8055
amendoxe <arturo_mendoza@outlook.com>
2022-11-10 11:53:05 -0600

    Clean a little bit

--- module/cuentaCorriente.js
+++ module/cuentaCorriente.js
@@ -8,30 +8,35 @@ export class cuentaCorriente {
 	set cliente(value) {
 		if (value instanceof cuentaCliente) this.#cliente = value;
 	}
+
 	get cliente() {
 		return this.#cliente;
 	}
+
 	constructor(cliente, numeroDeCuenta, agencia) {
 		this.#cliente = cliente;
 		this.numeroDeCuenta = numeroDeCuenta;
 		this.agencia = agencia;
 		this.#saldoDeCuenta = 0;
 	}
+
 	depositoEnCuenta(cantidad) {
 		if (cantidad > 0) this.#saldoDeCuenta += cantidad;
 		else console.log("No se puede depositar");
 		return this.#saldoDeCuenta;
 	}
+
 	retiroEnCuenta(cantidad) {
 		if (this.#saldoDeCuenta - cantidad >= 0) {
 			this.#saldoDeCuenta -= cantidad;
 			return this.#saldoDeCuenta;
 		} else console.log("saldo insuficiente");
-		//this.#saldoDeCuenta-=cantidad;
 	}
+
 	verSaldo() {
 		return this.#saldoDeCuenta;
 	}
+
 	transferirACuenta(cantidad, cuentaDestino) {
 		this.retiroEnCuenta(cantidad);
 		cuentaDestino.depositoEnCuenta(cantidad);
